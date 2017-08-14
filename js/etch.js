1083de258513b01a2a8e5a3e9501fdd9a396e265
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 21:24:11 -0500

    added div maker

--- js/etch.js
+++ js/etch.js
@@ -1,3 +1,15 @@
+//placeholder vars
+var userinput = 16;
+
 $(document).ready(function () {
 
 });
+
+function makeRows(userinput) {
+  var container = $(".container");
+  container.append("<div class=row>");
+  for (var i = 0; i === 16; i++) {
+    container.append("<div><p>test</p></div>"
+   }
+  container.append("</div>");
+ }
