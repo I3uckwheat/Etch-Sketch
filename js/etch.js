f2e6de62a2abba31aa612e58c8ccdf64a0f3604d
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 22:23:53 -0500

    removed comments

--- js/etch.js
+++ js/etch.js
@@ -9,9 +9,10 @@ $(document).ready(function () {
 function makeRows(input) {
   var container = $(".grid");
   var rowNumber = 1;
+
   container.append("<div class='row' id='row" + rowNumber + "'>");
-  //container.append("<p>append test</p>");
+
   for (var i = 1; i <= input; i++) {
-    container.find("#row" + rowNumber).append("<div class='block'><p>block</p></div>");
+    container.find("#row" + rowNumber).append("<div class='block'></div>");
   }
 }
