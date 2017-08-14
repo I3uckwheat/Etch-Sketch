502ef39e47bc01bf5226b182c4d44745b234508d
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 23:56:18 -0500

    cleaned up line 13. (Thanks Rhys (@105ron))

--- js/etch.js
+++ js/etch.js
@@ -10,7 +10,7 @@ function makeRows(input) {
   var rowNumber = 1;
 
   for (var i = 1; i <= input; i++) {
-    container.append("<div class='row' id='row" + rowNumber + "'>");
+    container.append(`<div class='row' id='row${ rowNumber }'>`);
     for (var j = 1; j <= input; j++) {
       container.find("#row" + rowNumber).append("<div class='block'></div>");
     }
