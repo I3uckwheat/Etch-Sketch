582e50d7125038b59126c500fbb6185c7bc7d13c
Briggs Elsperger <briggse@protonmail.com>
2017-08-13 21:35:42 -0500

    Makes colums

--- js/etch.js
+++ js/etch.js
@@ -2,14 +2,15 @@
 var userinput = 16;
 
 $(document).ready(function () {
-
+  makeRows(userinput);
 });
 
-function makeRows(userinput) {
-  var container = $(".container");
+
+function makeRows(input) {
+  var container = $(".grid");
   container.append("<div class=row>");
-  for (var i = 0; i === 16; i++) {
-    container.append("<div><p>test</p></div>"
+  for (var i = 1; i <= input; i++) {
+    container.append("<div><p>test</p></div>");
    }
   container.append("</div>");
  }
